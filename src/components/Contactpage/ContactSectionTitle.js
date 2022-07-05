8e117a1f14c33b85ab329c0e4993c11ebefa8f7c
uzumaffy <jovanie.sten2@gmail.com>
2022-07-05 15:07:08 +0200

    contactpageend

--- src/components/Contactpage/ContactSectionTitle.js
+++ src/components/Contactpage/ContactSectionTitle.js
@@ -2,5 +2,5 @@ import React from "react";
 import CardTitle from "../Card/CardTitle";
 
 export default function ContactSectionTitle() {
-  return <CardTitle littletitle="" title="Contactez moi" />;
+  return <CardTitle littletitle="" title="Contactez-moi" />;
 }
